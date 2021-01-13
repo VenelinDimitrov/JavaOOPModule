c725c2e8470f08c60361647d8c21b43351ffc2e2
VenelinDimitrov <65946212+VenelinDimitrov@users.noreply.github.com>
2021-01-13 19:01:56 +0200

    Need For Speed task - Java OOP
    There is no code included in the main method, because this was required from the testing system, as it tested the solution with its own main method.

---
+++ NeedForSpeed/NeedForSpeed/Vehicle.java
@@ -0,0 +1,45 @@
+package NeedForSpeed;
+
+public class Vehicle {
+    protected final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    protected double fuelConsumption;
+    protected double fuel;
+    protected int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public void drive(double kilometers) {
+        if (this.fuel >= this.fuelConsumption * kilometers){
+            this.fuel = this.fuel - (this.fuelConsumption * kilometers);
+        }
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public double getFuelConsumption() {
+        return this.fuelConsumption;
+    }
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+
+}
